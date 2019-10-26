d59db977635b62ebfc60275da12feb8aa99d83b7
mushfiqur-anik <mushfiquranik@mushfiqurs-MacBook-Pro.local>
2019-10-26 19:45:40 -0400

    Publishing Project

---
+++ fair/FairType.java
@@ -0,0 +1,10 @@
+package fair;
+
+/** 
+ * Written By: Mushfiqur Anik
+ * Description: This program manages different types of Events that happens in the city year round.
+ **/
+
+public enum FairType {
+	career, science, trade, travel;
+}
---
+++ sportsCompetition/SeasonName.java
@@ -0,0 +1,10 @@
+package sportsCompetition;
+
+/** 
+ * Written By: Mushfiqur Anik
+ * Description: This program manages different types of Events that happens in the city year round.
+ **/
+
+public enum SeasonName {
+	summer, fall, winter, spring;
+}
